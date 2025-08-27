5552ccbba800160ea4c0f1b6cb69f30f598fe56d
fedoraiver <cck1772416022@gmail.com>
2025-08-27 15:05:03 +0800

    feat: 移除 IsTilting 删除时的 mesh 重置逻辑
    - 删除 RemovedComponents<IsTilting> 相关代码
    - 卡牌与阴影在取消倾斜时不再强制重置顶点
    - 简化 tilt_card 系统逻辑

--- src/game_play/systems/tilting.rs
+++ src/game_play/systems/tilting.rs
@@ -10,10 +10,8 @@ const RESET_FACTOR: f32 = 0.95;
 
 pub fn tilt_card(
     query1: Query<(Entity, &Mesh2d, &IsTilting), (With<Card>, Changed<IsTilting>)>,
-    query2: Query<(Entity, &Mesh2d), (With<Card>, Without<IsTilting>)>,
     query3: Query<&Children>,
     query4: Query<&Mesh2d, With<CardShadow>>,
-    mut removals: RemovedComponents<IsTilting>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
     for (entity, mesh2d, is_tilting) in query1.iter() {
@@ -127,56 +125,6 @@ pub fn tilt_card(
                                     pos[0] = x + (pos[0] - x) * RESET_FACTOR;
                                     pos[1] = y + (pos[1] - y) * RESET_FACTOR;
                                 }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for entity in removals.read() {
-        let (entity, mesh2d) = query2.get(entity).unwrap();
-        if let Some(card_mesh) = meshes.get_mut(mesh2d) {
-            if let Some(VertexAttributeValues::Float32x3(positions)) =
-                card_mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION)
-            {
-                for pos in positions.iter_mut() {
-                    let x = if pos[0] > 0.0 {
-                        CARD_WIDTH / 2.0
-                    } else {
-                        -CARD_WIDTH / 2.0
-                    };
-                    let y = if pos[1] > 0.0 {
-                        CARD_HEIGHT / 2.0
-                    } else {
-                        -CARD_HEIGHT / 2.0
-                    };
-                    pos[0] = x;
-                    pos[1] = y;
-                }
-            }
-        }
-        if let Ok(children) = query3.get(entity) {
-            for child in children.iter() {
-                if let Ok(mesh2d) = query4.get(child) {
-                    if let Some(shadow_mesh) = meshes.get_mut(mesh2d) {
-                        if let Some(VertexAttributeValues::Float32x3(positions)) =
-                            shadow_mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION)
-                        {
-                            for pos in positions.iter_mut() {
-                                let x = if pos[0] > 0.0 {
-                                    CARD_WIDTH / 2.0
-                                } else {
-                                    -CARD_WIDTH / 2.0
-                                };
-                                let y = if pos[1] > 0.0 {
-                                    CARD_HEIGHT / 2.0
-                                } else {
-                                    -CARD_HEIGHT / 2.0
-                                };
-                                pos[0] = x;
-                                pos[1] = y;
                             }
                         }
                     }
